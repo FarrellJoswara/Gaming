719a3720276d4c0c58db711dbacdd91b018a1c57
AYY Yung <99507808+ltcolscotty@users.noreply.github.com>
2022-04-05 17:05:31 +0000

    Extra - The

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-val1 = input("Test prompt")
-print ("value 1 is" + val1)
+import random2
+val1 = ""
+if input("1 or 2 ") == "1": 
+  while val1 != "quit":
+      val1 = input("Enter value 1: ")
+      print("value 1 is " + val1)
+else:
+  num = random2.randint(0,10)
+  if input("pick a number between 0 and 10: " ) == str(num):
+    print("congrats you got it")
+  else: 
+    print("so bad the number was " + str(num))
+
+
+
+
