ec03f7edcd17b92d02b2b96167c9993290f00ed1
Aidan Y <99507808+ltcolscotty@users.noreply.github.com>
2022-04-08 18:35:22 +0000

    TALKING BEN!!!

---
+++ TalkingBen.py
@@ -0,0 +1,21 @@
+import random2
+text = input("Brian? ")
+num = random2.randint(1,4)
+
+if num == "1":
+  print("Yes?")
+
+elif num == "2":
+      print("No.")
+      
+elif num == "3":
+  print("ho ho ho!")
+
+elif num == "4":
+  print("auaueeuguh")
+
+
+
+
+
+
