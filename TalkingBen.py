be9211d0030ad40e9ddf895f492a989c1d6edcca
Aidan Y <99507808+ltcolscotty@users.noreply.github.com>
2022-04-08 18:46:04 +0000

    Improved response

--- TalkingBen.py
+++ TalkingBen.py
@@ -1,19 +1,21 @@
 import random2
 text = input("Brian? ")
-num = random2.randint(1,4)
 
-if num == "1":
-  print("Yes?")
+while text != "SHUT UP":
+  num = random2.randint(1,4)
 
-elif num == "2":
-      print("No.")
-      
-elif num == "3":
-  print("ho ho ho!")
-
-elif num == "4":
-  print("auaueeuguh")
-
+  if num == "1":
+    print("Yes?")
+    text = input("Brian? ")
+  elif num == "2":
+    print("No.")
+    text = input("Brian? ")
+  elif num == "3":
+    print("ho ho ho!")
+    text = input("Brian? ")
+  elif num == "4":
+    print("auaueeuguh")
+    text = input("Brian? ")
 
 
 
